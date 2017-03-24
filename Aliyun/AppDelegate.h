b0c86514102bf35f1c084a0132f0f16e04e9e2e8
weiweilong <weiweilong_email@foxmail.com>
2017-03-24 09:00:48 +0800

    Initial Commit

---
+++ Aliyun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aliyun
+//
+//  Created by 魏唯隆 on 2017/3/24.
+//  Copyright © 2017年 魏唯隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Aliyun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Aliyun
+//
+//  Created by 魏唯隆 on 2017/3/24.
+//  Copyright © 2017年 魏唯隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
